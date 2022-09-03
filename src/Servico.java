bdf120e2c488a108a3c070e74974aaf5b2011bd6
gcostacoelho <gcostacoelho2003@gmail.com>
2022-09-03 18:55:37 -0300

    create-class

---
+++ src/Servico.java
@@ -0,0 +1,30 @@
+public class Servico {
+    private String sigla, nome, desc;
+    private boolean status;
+
+    public String getSigla() { return sigla; }
+    
+    public void setSigla(String sigla) { this.sigla = sigla; }
+    
+    public String getNome() { return nome; }
+    
+    public void setNome(String nome) { this.nome = nome; }
+    
+    public String getDesc() { return desc; }
+    
+    public void setDesc(String desc) { this.desc = desc; }
+    
+    public boolean isStatus() { return status; }
+
+    public void setStatus(boolean status) { this.status = status; }
+
+    public Servico(String sigla, String nome, String desc, boolean status) {
+        this.sigla = sigla;
+        this.nome = nome;
+        this.desc = desc;
+        this.status = status;
+    }
+
+    
+    
+}
