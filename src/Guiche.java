e4d86342fc139af92a3770f4411abca190a8fb58
Rezende-Fabio <rezendefabio337@gmail.com>
2022-09-28 15:25:32 -0300

    Add class Guiche

---
+++ src/Guiche.java
@@ -0,0 +1,36 @@
+public class Guiche {
+    private int numeroGui;
+    private String descricaoGui;
+
+    public int trocarGuiche(){
+        return numeroGui;
+    }
+
+
+    public int getNumeroGui() {
+        return numeroGui;
+    }
+
+
+    public void setNumeroGui(int numeroGui) {
+        this.numeroGui = numeroGui;
+    }
+
+
+    public String getDescricaoGui() {
+        return descricaoGui;
+    }
+
+
+    public void setDescricaoGui(String descricaoGui) {
+        this.descricaoGui = descricaoGui;
+    }
+
+
+    public Guiche(int numeroGui, String descricaoGui) {
+        this.numeroGui = numeroGui;
+        this.descricaoGui = descricaoGui;
+    }
+
+    
+}
